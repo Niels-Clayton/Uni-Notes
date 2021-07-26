8b69757cb7c282b0dbf2a895d9099cd1083d0d25
Niels Clayton <nielsdanielclayton@gmail.com>
2021-07-26 23:28:37 +1200

    some shit

--- Tri 2/ECEN415/Assignment 1/Assignment_1_Summative.m
+++ Tri 2/ECEN415/Assignment 1/Assignment_1_Summative.m
@@ -2,10 +2,38 @@
 clc;
 clear;
 
-t_d=-1;
+t_d=-0.5;
 s = tf('s');
 
 D = exp(s*t_d);
 
 figure(1);
 bode(D);
+
+%% 
+clc;
+clear;
+
+s = tf('s');
+
+figure(1)
+
+D = exp(s * -0.00027);
+D_pade = pade(D, 1);
+
+bode(D_pade);
+legend("Delay = 270\mu s");
+
+%%
+clc;
+clear;
+
+s = tf('s');
+
+figure(1)
+
+D = exp(s * -0.00026);
+D_pade = pade(D, 2);
+
+bode(D_pade);
+legend("Delay = 261\mu s");
