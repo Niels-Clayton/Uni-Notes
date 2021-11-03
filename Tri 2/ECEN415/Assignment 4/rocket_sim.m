41fd461b8aeaccada3d41ed330e5d2de2d215a16
Niels-Clayton <nielsdanielclayton@gmail.com>
2021-11-03 20:06:57 +1300

    415 assignment 4 complete, holy shit it sucked, one more thing to go and my honours is done

--- Tri 2/ECEN415/Assignment 4/rocket_sim.m
+++ Tri 2/ECEN415/Assignment 4/rocket_sim.m
@@ -1,6 +1,4 @@
 % Nonlinear rocket simulation.
-
-clc; clear;
 global g c_d M J L eta k
 
 g = -9.8;   
@@ -45,7 +43,7 @@ f = @(t,x) [x(2);
             x(8)*sin(deg2rad(x(5))) / (M+x(7));
             x(6);
             k/(J+L^2*x(7))*x(9);
-            -1/eta*(x(8)+x(9));
+            -1/eta*(x(8)+abs(x(9)));
             0;
             0];
         
@@ -73,6 +71,7 @@ t_store = [];
 % Begin the simulation
 figure(1)
 clf;
+
 while (x(1) > -0.01) & (elapsed_time <  sim_time)
     
     if x(7) >  0 % Check whether there is any fuel remaining.
@@ -119,8 +118,6 @@ plot(t_store, u_store(:,2))
 xlabel('Time [s]')
 ylabel('u_2')
 
-x(5)
-
 %--------------------------------------------------------------------------
 % Plotting function to display the telemetry during flight.
 %--------------------------------------------------------------------------
@@ -139,7 +136,7 @@ function[] = plot_telemetry(t,x)
   ylabel('Velocities [m/s]')
   hold on
   plot(t, x(:,2), 'b', t, x(:,4), 'r')
-  legend('Vertical speed', 'Horizontal speed', 'Location', 'northwest')
+%   legend('Vertical speed', 'Horizontal speed', 'Location', 'northwest')
   
   subplot(4,2,6)
   xlabel('Time [s]')
@@ -172,6 +169,76 @@ function[] = plot_telemetry(t,x)
   
 end
 
+
+
+%--------------------------------------------------------------------------
+% Static open loop controller that simply sets each input to pre-defined
+% values at given time steps
+%--------------------------------------------------------------------------
+function[u1, u2] = static_control(t)
+    if t < 6
+        u1 = 92000;
+        u2 = 0;
+    elseif t < 12
+        u1 = 55000;
+        u2 = 1790;
+    else 
+        u1 = 22000;
+        u2 = -3000;
+    end
+end
+
+%--------------------------------------------------------------------------
+% Linerised open loop controller. Uses Jacobians to form a linerised model
+% of the system at each time step. and uses this model to calculate the
+% required input to reach a provided target state
+%--------------------------------------------------------------------------
+function[u1, u2] = open_loop_linearisation(A, B, cur_x, t)
+    
+    % Target state of the rocket
+    if t <= 5
+        target_x = [(t+1)*1500/15, 300, 0, 0, 0, 0, 1000-((t+1)*(1000/15))]';
+    else
+        target_x = [(t+1)*1500/15, 300, 50, 5, 10, 4-((t+1)*(4/10)), 1000-((t+1)*(1000/15))]';
+    end
+    
+    % Calculate the inputs required to achieve the desired state
+    u = pinv(B)*(target_x - cur_x - A*cur_x);
+
+    % Clamp the calculated inputs
+    if u(1,:) < 0
+        u1 = 0;
+    else
+        u1 = u(1,:);
+    end
+
+    if u(2,:) < 0
+        u2 = 0;
+    else
+        u2 = u(2,:);
+    end
+end
+
+
+function[u1, u2] = closed_loop(A, B, cur_x, cur_u, t)
+    
+    load("x_target.mat");
+    target_x = x_target(t+1,:)';
+
+    x_bounds = [0.1; 0.16; 1; 0.1; 0.05; 0.05; 16];
+    u_bounds = [7.8; 50];
+
+    Q = diag((x_bounds.^2)./((target_x+1e-9).^2));
+    R = diag(u_bounds./(cur_u+1e-6).^2); % Ensure that the current u1 != 0
+
+    [K,~,~] = lqr(A, B, Q, R);
+    u = -K*(cur_x - target_x);
+    
+    u1 = min(max(u(1, :), 1e-3), 100000);
+    u2 = min(max(u(2, :), -10000), 10000);
+end
+
+
 %--------------------------------------------------------------------------
 % Controller code
 %--------------------------------------------------------------------------
@@ -179,71 +246,52 @@ end
 % here is not particularly clever, but should run.
 
 function[u1, u2] = controller_command(t, x)
-    global g c_d M J L eta k x_0
+    global g c_d M J L eta k linearisation open_loop_control
     persistent Df_x Df_u 
-    
-    % Final states we wish to achieve
-    x_f = [1500, 300, 100, 20, 10, 0, 0];  
-    
-    
-    % Set the initial inputs to the system, these have been placed inside
-    % the x vector for t>0.
-    % Also create the system Jacobians based on a non-linear system model
+    % % Linerisation ------------------------------------------------------
+    % Calculate the Jacobain approximation on the first time step 
     if t == 0
         % Set the initial conditions of the system
-        x = [x, 0, 0]; 
+        x = [x, 50040, 0]; 
         
-        % % Symbolic Model --------------------------------------------------------
         syms x1 x2 x3 x4 x5 x6 x7 u1 u2
-        f_sym = @(t,x) [x2; 
-                        g + u1*cos(x5) / (M+x7) - c_d*x2^2;  
-                        x4;
-                        u1*sin(x5) / (M+x7);
-                        x6;
-                        k/(J+L^2*x7)*u2;
-                        -1/eta*(u1+u2);
-                        ];
-        % % Symbolic Model --------------------------------------------------------
+        f_sym = [x2; 
+                 g + u1*cos(x5) / (M+x7) - c_d*x2^2;  
+                 x4;
+                 u1*sin(x5) / (M+x7);
+                 x6;
+                 k/(J+L^2*x7)*u2;
+                 -1/eta*(u1+abs(u2));
+                 ];
         
         % Generate the jacobian matrecies
         Df_x = jacobian(f_sym, [x1 x2 x3 x4 x5 x6 x7]);
         Df_u = jacobian(f_sym, [u1 u2]);
     end
-    
-    
-    % Substitute values into the A matrix and B matrix Jacobians from the
-    % previous time step
-    syms x1 x2 x3 x4 x5 x6 x7 u1 u2
-    A = double(subs(Df_x, [x1 x2 x3 x4 x5 x6 x7 u1 u2], x));
-    B = double(subs(Df_u, [x1 x2 x3 x4 x5 x6 x7 u1 u2], x));
-    
-    % Somehow calculate the next u1 and u2???
-    u1 = eta*x_0(7)/15; % Burn all of the fuel in 15s at uniform rate. 
-    u2 = eta*2;  
-    
+    % % Linerisation ------------------------------------------------------ 
+
     % Current state of the rocket    
-    cur_x = x(:,1:7)'; %current state of x 
-    cur_u = x(:,8:9)'; %current state of u 
+    cur_x = x(:,1:7)'; % current state of x 
+    cur_u = x(:,8:9)'; % current state of u
+
+    % If using a linerised controller, substitute the current operating point
+    % into the Jacobian A and B
+    if linearisation        
+        syms x1 x2 x3 x4 x5 x6 x7 u1 u2
+        A = double(subs(Df_x, [x1 x2 x3 x4 x5 x6 x7 u1 u2], x));
+        B = double(subs(Df_u, [x1 x2 x3 x4 x5 x6 x7 u1 u2], x));
+        
+        % Select the controller type to use (This is done externally)
+        if open_loop_control
+            [u1, u2] = open_loop_linearisation(A, B, cur_x, t);
+        else
+            [u1, u2] = closed_loop(A, B, cur_x, cur_u, t);
+        end
     
-    % Desired state of the rocket
-    target_x = [(t+1)*10000/15, 130, 0, 0, 0, 0, 1000-((t+1)*(1000/15))]';
-    % new_x = cur_x + A*cur_x + B*[u1, u2]';
-    
-    % Calculate the unputs required to achieve the desired state
-    u = pinv(B)*(target_x - cur_x - A*cur_x);
-    
-    if u(1,:) < 0
-        u1 = 0;
-    else
-        u1 = u(1,:);
-    end
-    
-    if x(:,5) >= 5 % turn off at 6deg
-        u2 = 0;
-    else
-        u2 = 400;
-    end
-    
+    % Use an open loop fuzzy logic static controller
+    elseif open_loop_control
+        [u1, u2] = static_control(t);
+    end    
 end
 
 % END =========================================================================
